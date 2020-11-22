2e80ec41a19bdad6c24a97d973dc103cf962ea45
Uneven Coder <ameenshafeeq2005@gmail.com>
2020-11-22 12:34:24 +0300

    done with basic functionality

--- script.js
+++ script.js
@@ -1,9 +1,96 @@
 document.addEventListener("DOMContentLoaded", Loaded);
-function generateCard(num) {
+function generateCard() {
     const container = document.querySelector('#container')
-    let number = 500/num+'px'
-    container.innerHTML += '<div width='+number+' height='+number+' class="box">Box</div>'
+    for(let i = 0 ; i< 36 ; i++ )
+    {
+        container.innerHTML += '<div id='+i+'  onmouseover="changeColor(this)" style=\'width:100px;height:100px\' class="box"></div>'   
+    }
 }
+
+function GreyScale(x){
+    let id = x.id
+    if(document.getElementById(id).style.backgroundColor!= ''){
+                if(document.getElementById(id).style.backgroundColor.split(',')[3]){
+        let a = document.getElementById(id).style.backgroundColor.split(',')[3]
+        let str=''
+        for(let i = 0 ; i < a.length-1 ;i++){
+    str+=a[i] 
+        }
+    let alpha = parseFloat(str)<0.98?parseFloat(str)+0.04:parseFloat(str)>=0.08?0.2:0.2
+    document.getElementById(id).style.backgroundColor=`rgba(0,0,0,${alpha})`
+      
+                }
+                else{
+    document.getElementById(id).style.backgroundColor=`rgba(0,0,0,0.2)`
+                }
+
+}
+else{
+    document.getElementById(id).style.backgroundColor=`rgba(0,0,0,0.2)`
+      
+}
+}
+function Eraser(x){
+    id=x.id
+    document.getElementById(id).style.backgroundColor='white';
+}
+function Black(x){
+    id=x.id
+    document.getElementById(id).style.backgroundColor='black';
+}
+function RainBow(x){
+    let id = x.id,color=1
+    let r = Math.floor(Math.random() * 255)
+    let g = Math.floor(Math.random() * 255)
+    let b = Math.floor(Math.random() * 255)
+
+    if(document.getElementById(id).style.backgroundColor!= ''){
+if(document.getElementById(id).style.backgroundColor.split(',')[3]){
+    let a = document.getElementById(id).style.backgroundColor.split(',')[3]
+    let str=''
+    for(let i = 0 ; i < a.length-1 ;i++){
+str+=a[i] 
+    }
+let alpha = parseFloat(str)<0.8?parseFloat(str)+0.2:parseFloat(str)>=0.08?0.2:0.2
+document.getElementById(id).style.backgroundColor=`rgba(${r},${g},${b},${alpha})`
+}
+else{
+    document.getElementById(id).style.backgroundColor=`rgba(`+r+`,`+g+`,`+b+`,0.2)`
+}
+}else{
+    document.getElementById(id).style.backgroundColor=`rgba(${r},${g},${b},0.2)`
+}
+}
+
+function Color(x) {
+    let id = x.id;
+    let color = document.getElementById('cp').value
+    document.getElementById(id).style.backgroundColor = color;
+}
+
 function Loaded() {
-    generateCard(10)
+    localStorage.setItem('state', 'Black');
+    generateCard()
+}
+function changeColor(x){
+    let state = localStorage.getItem('state')
+    if (state == 'GreyScale') {
+        GreyScale(x)
+    }
+    else if (state == 'Rainbow') {
+        RainBow(x)
+    }
+    else if (state == 'Eraser') {
+        Eraser(x)
+    }
+    else if (state == 'Black') {
+        Black(x)
+    }
+    else if (state == 'Color') {
+        Color(x)
+    }
+}
+
+function changeState(state){
+    localStorage.setItem('state', state);
 }
